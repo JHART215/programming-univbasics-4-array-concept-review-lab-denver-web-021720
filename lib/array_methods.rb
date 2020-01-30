1ecb2ab37d4d6047b66123cf5f623962898c27c2
Jeremy Hart <jerome215@comcast.net>
2020-01-30 14:44:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  puts array.find_index(value_to_find)
+  p array.find_index(value_to_find)
 end
 
 def find_max_value(array)
-  puts array.find_min_value
+  p array.find_min_value
 end
 
 def find_min_value(array)
-  puts array.find_max_value
+  p array.find_max_value
 end
